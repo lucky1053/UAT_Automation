cf0293d4d5a0b65c71395a24f7cd95d092e6fcab
lucky1053 <jain.lucky1053@gmail.com>
2022-06-29 09:53:58 -0700

    added line in DMI file

--- src/test/java/DMI_Automation.java
+++ src/test/java/DMI_Automation.java
@@ -2,5 +2,6 @@ public class DMI_Automation {
     public static void main(String[] args) {
 
         System.out.println("Hi this is DMI application.");
+        System.out.println("This line is added by lucky1053");
     }
 }
