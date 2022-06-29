bb35c20e2477e5f03526a177e0df0bc254c1a012
lucky1053 <jain.lucky1053@gmail.com>
2022-06-29 09:56:01 -0700

    added second line in DMI file

--- src/test/java/DMI_Automation.java
+++ src/test/java/DMI_Automation.java
@@ -3,5 +3,7 @@ public class DMI_Automation {
 
         System.out.println("Hi this is DMI application.");
         System.out.println("This line is added by lucky1053");
+        System.out.println("This second line is added by lucky1053");
+
     }
 }
